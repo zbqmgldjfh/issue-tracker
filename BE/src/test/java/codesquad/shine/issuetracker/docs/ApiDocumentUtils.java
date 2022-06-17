25db5c8ee8bcfd8e16b0e75671444a2fd62b9479
jiwoo <zbqmgldjfh@gmail.com>
2022-06-17 14:33:03 +0900

    feat(ApiDocumentUtils): RestDocs 의 깔끔한 출력을 위한 pretty 설정

---
+++ BE/src/test/java/codesquad/shine/issuetracker/docs/ApiDocumentUtils.java
@@ -0,0 +1,20 @@
+package codesquad.shine.issuetracker.docs;
+
+import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
+import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+
+public class ApiDocumentUtils {
+
+    private ApiDocumentUtils() {
+    }
+
+    public static OperationRequestPreprocessor getDocumentRequest() {
+        return preprocessRequest(prettyPrint());
+    }
+
+    public static OperationResponsePreprocessor getDocumentResponse() {
+        return preprocessResponse(prettyPrint());
+    }
+}
