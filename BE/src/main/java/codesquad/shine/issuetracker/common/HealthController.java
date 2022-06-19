be09cb9b4e09016f932f1df69e2612422c245096
jiwoo <zbqmgldjfh@gmail.com>
2022-06-19 17:02:50 +0900

    feat(HealthController): 서버 상태 확인용 api

---
+++ BE/src/main/java/codesquad/shine/issuetracker/common/HealthController.java
@@ -0,0 +1,13 @@
+package codesquad.shine.issuetracker.common;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "OK!";
+    }
+}
