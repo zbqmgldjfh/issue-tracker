ef953f0c3a7359a309b96849a8bd464f1aa7f906
jiwoo <zbqmgldjfh@gmail.com>
2022-06-22 19:55:55 +0900

    chore(IssueQueryRepository): 원래는 DTO 전용 Repository 였는데, 반환을 DTO가 아닌, Entity로 하고 있었다. 따라서 삭제하였다.

--- BE/src/main/java/codesquad/shine/issuetracker/issue/domain/IssueRepository.java
+++ BE/src/main/java/codesquad/shine/issuetracker/issue/domain/IssueRepository.java
@@ -1,6 +1,26 @@
 package codesquad.shine.issuetracker.issue.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface IssueRepository extends JpaRepository<Issue, Long>, IssueQueryRepository {
+import java.util.Optional;
+
+public interface IssueRepository extends JpaRepository<Issue, Long> {
+
+    @Query("select i from Issue i " +
+            "join fetch i.author u " +
+            "join fetch i.milestone m " +
+            "where i.id = :id")
+    Optional<Issue> optimizationFindById(@Param("id") Long id);
+
+    @Query(value = "select i from Issue i " +
+            "join fetch i.author u " +
+            "join fetch i.milestone m " +
+            "where i.open = :open",
+            countQuery = "select count(i.id) from Issue i where i.open = :open"
+    )
+    Page<Issue> optimizationFindIssueByStatus(@Param("open") Boolean open, Pageable pageable);
 }
