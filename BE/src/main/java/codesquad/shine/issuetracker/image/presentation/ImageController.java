51266e0d896a70b6023c7083033015d3d558ccfe
jiwoo <zbqmgldjfh@gmail.com>
2022-06-23 13:52:21 +0900

    feat(ImageController): ImageController 구현

--- BE/src/main/java/codesquad/shine/issuetracker/image/presentation/ImageController.java
+++ BE/src/main/java/codesquad/shine/issuetracker/image/presentation/ImageController.java
@@ -1,17 +1,29 @@
 package codesquad.shine.issuetracker.image.presentation;
 
+import codesquad.shine.issuetracker.auth.annotation.ForLoginUser;
+import codesquad.shine.issuetracker.image.business.ImageService;
+import codesquad.shine.issuetracker.image.domain.Image;
+import codesquad.shine.issuetracker.image.presentation.dto.ImageResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/images")
 public class ImageController {
 
-    @PostMapping
-    public void upload(@RequestParam("file") MultipartFile file) {
+    private final ImageService imageService;
 
+    @ForLoginUser
+    @PostMapping
+    public ImageResponse upload(@RequestParam("file") MultipartFile file) throws IOException {
+        Image savedImg = imageService.upload(file);
+        return new ImageResponse(savedImg.getImgUrl());
     }
 }
