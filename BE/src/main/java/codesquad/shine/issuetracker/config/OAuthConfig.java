abfdb2008bbf716022cc5eb7343f3684ee601408
jiwoo <zbqmgldjfh@gmail.com>
2022-06-23 13:42:02 +0900

    feat(S3Properties): AWS 설정정보를 받는 S3Properties 객체 생성하기

--- BE/src/main/java/codesquad/shine/issuetracker/config/OAuthConfig.java
+++ BE/src/main/java/codesquad/shine/issuetracker/config/OAuthConfig.java
@@ -4,6 +4,7 @@ import codesquad.shine.issuetracker.auth.InMemoryProviderRepository;
 import codesquad.shine.issuetracker.auth.OAuthAdapter;
 import codesquad.shine.issuetracker.auth.OAuthProperties;
 import codesquad.shine.issuetracker.auth.OAuthProvider;
+import codesquad.shine.issuetracker.image.infra.S3Properties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +14,7 @@ import java.util.Map;
 
 @Configuration
 @RequiredArgsConstructor
-@EnableConfigurationProperties(OAuthProperties.class)
+@EnableConfigurationProperties({OAuthProperties.class, S3Properties.class})
 public class OAuthConfig {
 
     private final OAuthProperties properties;
