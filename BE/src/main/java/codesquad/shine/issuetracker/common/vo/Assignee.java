4b1cac9d67a63ad787f26bd215868e1b3278e395
jiwoo <zbqmgldjfh@gmail.com>
2022-06-24 19:39:08 +0900

    feat(Assignee): VO인 Assignee를 TDD로 구현하기

---
+++ BE/src/main/java/codesquad/shine/issuetracker/common/vo/Assignee.java
@@ -0,0 +1,37 @@
+package codesquad.shine.issuetracker.common.vo;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@EqualsAndHashCode(of = "userId")
+public class Assignee {
+
+    private static final String INVALID_RANGE_ID = "회원 아이디는 1부터 가능합니다.";
+    private static final String INAVLID_LENGTH_NAME = "회원 이름의 길이는 1이상 이여야만 합니다.";
+    private final Long userId;
+    private final String userName;
+    private final String avatarUrl;
+    private final boolean isAssigned;
+
+    public Assignee(Long userId, String userName, String avatarUrl, boolean isAssigned) {
+        isValidRangeId(userId);
+        isValidLengthName(userName);
+        this.userId = userId;
+        this.userName = userName;
+        this.avatarUrl = avatarUrl;
+        this.isAssigned = isAssigned;
+    }
+
+    private void isValidRangeId(Long userId) {
+        if (userId <= 0) {
+            throw new IllegalArgumentException(INVALID_RANGE_ID);
+        }
+    }
+
+    private void isValidLengthName(String userName) {
+        if (userName == null || userName.isBlank()) {
+            throw new IllegalArgumentException(INAVLID_LENGTH_NAME);
+        }
+    }
+}
