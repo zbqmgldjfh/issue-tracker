ddcdbea271ee88fb52242ad8e93c72cc76b07a23
bukim0329 <bukim0329@gmail.com>
2022-06-24 12:24:08 +0900

    feat: 탭 사라지는 기능 추가

--- frontend/src/components/issues/DropdownButton.tsx
+++ frontend/src/components/issues/DropdownButton.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
+import { RootState } from 'src';
 import DropdownCaret from 'src/assets/DropdownCaret.svg';
-import { changeTab } from 'src/modules/clickedTab';
+import { changeTab, deleteTab } from 'src/modules/clickedTab';
 import { ButtonBox, Button } from './DropdownButton.styled';
 import DropdownMenu from './DropdownMenu';
 
@@ -13,10 +15,15 @@ type propsType = {
 
 export default function DropdownButton({ modalWidth, text, makeItem }: propsType) {
   const dispatch = useDispatch();
+  const clickedTab = useSelector((state: RootState) => state.clickedTab);
+  const btnEvent = (e: any) => {
+    if (e.target.closest('[data-buttonname]').dataset.buttonname === clickedTab.clickedTab) dispatch(deleteTab());
+    else dispatch(changeTab(text));
+  };
 
   return (
     <ButtonBox>
-      <Button onClick={() => dispatch(changeTab(text))}>
+      <Button onClick={btnEvent} data-buttonname={text}>
         {text}
         <DropdownCaret />
       </Button>
