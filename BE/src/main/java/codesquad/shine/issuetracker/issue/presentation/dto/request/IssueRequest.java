570df6b2f57ef84c822e3a21a618a5e6bd647ff7
jiwoo <zbqmgldjfh@gmail.com>
2022-06-25 20:44:06 +0900

    test(IssueControllerTest): Issue를 생성하는 service, controller 테스트 구현

--- BE/src/main/java/codesquad/shine/issuetracker/issue/presentation/dto/request/IssueRequest.java
+++ BE/src/main/java/codesquad/shine/issuetracker/issue/presentation/dto/request/IssueRequest.java
@@ -1,5 +1,6 @@
 package codesquad.shine.issuetracker.issue.presentation.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,10 +9,12 @@ import java.util.List;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class IssueRequest {
 
     @NotBlank
     private String title;
+
     private String comment;
     private List<Long> assigneeIds;
     private List<Long> labelIds;
