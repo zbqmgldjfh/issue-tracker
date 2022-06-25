4b75aace258be184d56823660709ba8a428456e0
jiwoo <zbqmgldjfh@gmail.com>
2022-06-25 16:55:36 +0900

    feat(IssueAssignee): User에게 Issue를 할당할때 IssueAssignee를 사용하도록 User->IssueAssginee의 연관관계를 만들어줌, 양방향이 됨

--- BE/src/main/java/codesquad/shine/issuetracker/issue/domain/Issue.java
+++ BE/src/main/java/codesquad/shine/issuetracker/issue/domain/Issue.java
@@ -4,12 +4,15 @@ import codesquad.shine.issuetracker.comment.domain.Comment;
 import codesquad.shine.issuetracker.common.imbeddable.BaseTimeEntity;
 import codesquad.shine.issuetracker.label.domain.Label;
 import codesquad.shine.issuetracker.milestone.domain.Milestone;
+import codesquad.shine.issuetracker.user.domain.User;
 import lombok.*;
 import org.springframework.util.Assert;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+
+import static javax.persistence.FetchType.LAZY;
 
 @Getter
 @Entity
@@ -26,11 +29,18 @@ public class Issue extends BaseTimeEntity {
 
     private Boolean open;
 
+    @JoinColumn
+    @ManyToOne(fetch = LAZY)
+    private User author;
+
+    @OneToMany(mappedBy = "issue", cascade = CascadeType.ALL)
+    private List<IssueAssignee> issueAssignees = new ArrayList<>();
+
     @OneToMany(mappedBy = "issue", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
 
     @JoinColumn(name = "milestone_id")
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = LAZY)
     private Milestone milestone;
 
     @ManyToMany
@@ -45,11 +55,18 @@ public class Issue extends BaseTimeEntity {
                 .build();
     }
 
+    public Issue(String title, Boolean open, User author) {
+        this.title = title;
+        this.open = open;
+        this.author = author;
+    }
+
     @Builder
-    public Issue(String title, boolean open, Milestone milestone) {
+    public Issue(String title, boolean open, User author, Milestone milestone) {
         Assert.hasText(title, "title must not be null");
         this.title = title;
         this.open = open;
+        this.author = author;
         this.milestone = milestone;
     }
 
@@ -78,5 +95,23 @@ public class Issue extends BaseTimeEntity {
     public void deleteComment(Comment comment) {
         this.comments.remove(comment);
         comment.deleteIssue();
+    }
+
+    public void addMilestone(Milestone milestone) {
+        if (milestone == null) {
+            return;
+        }
+        this.milestone = milestone;
+        milestone.getIssues().add(this);
+    }
+
+    public void addAssignees(List<User> assigneeList) {
+        assigneeList.forEach(assignee -> {
+            this.issueAssignees.add(new IssueAssignee(this, assignee));
+        });
+    }
+
+    public void addLabels(List<Label> labelList) {
+        labelList.forEach(label -> addLabel(label));
     }
 }
