2a7afa8c5149b4d48741d1524e2fc4e10de1189f
jiwoo <zbqmgldjfh@gmail.com>
2022-06-27 11:51:29 +0900

    chore(test): 테스트 관련 파일 주석처리

--- BE/src/test/java/codesquad/shine/issuetracker/IssueTrackerApplicationTests.java
+++ BE/src/test/java/codesquad/shine/issuetracker/IssueTrackerApplicationTests.java
@@ -1,12 +1,8 @@
 package codesquad.shine.issuetracker;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class IssueTrackerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 }
