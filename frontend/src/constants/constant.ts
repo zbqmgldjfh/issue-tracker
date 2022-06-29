af129a9a1e244bad27c9215b513824522679bbe3
youryu0212 <youryu0212@naver.com>
2022-06-29 14:17:13 +0900

    feat: 로그인 요청 성공시 로컬스토리지에 토큰 저장후 이슈페이지로 이동

---
+++ frontend/src/constants/constant.ts
@@ -0,0 +1,2 @@
+export const SERVER_URL = 'http://43.200.18.245';
+export const OAUTH_API_URL = '/api/auth/github/callback';
--- frontend/src/pages/loading/LoadingPage.tsx
+++ frontend/src/pages/loading/LoadingPage.tsx
@@ -1,8 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Loading from 'src/assets/loading.gif';
+import { OAUTH_API_URL, SERVER_URL } from 'src/constants/constant';
+import { fetchData } from 'src/utils/utils';
 import { LoadingBox, LoadingText } from './LoadingPage.styled';
 
+const parseOAuthCode = () => {
+  const [_, code] = location.search.split('=');
+
+  return code;
+};
+
+const getUrl = () => {
+  const oauthCode = parseOAuthCode();
+  const url = `${SERVER_URL}${OAUTH_API_URL}?code=${oauthCode}`;
+  return url;
+};
+
+const getToken = async (url: string) => {
+  console.log('fetch url :', url);
+  const data = await fetchData(url);
+  return data;
+};
+
 export default function LoadingPage() {
+  const [token, setToken] = useState('');
+  const navigate = useNavigate();
+  const url = getUrl();
+
+  useEffect(() => {
+    const runLogin = async () => {
+      const { token } = await getToken(url);
+      if (token) {
+        localStorage.setItem('token', JSON.stringify(token));
+        setToken(token);
+      }
+    };
+
+    runLogin();
+  }, []);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      navigate(`/`);
+    }
+  }, [token]);
+  getToken(url);
+
   return (
     <LoadingBox>
       <img src={Loading} alt="로딩중"></img>
