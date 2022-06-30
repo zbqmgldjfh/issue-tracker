da3fa73a24de521d1f456ad12483c7caafeb8c84
youryu0212 <youryu0212@naver.com>
2022-06-30 14:00:40 +0900

    feat: issues 리스트 서버와 연동해서 렌더링

--- frontend/src/components/issues/IssuesMain.tsx
+++ frontend/src/components/issues/IssuesMain.tsx
@@ -1,58 +1,51 @@
-import React from 'react';
-import { createKey } from 'src/utils/utils';
+import React, { useEffect, useState } from 'react';
+import { ISSUES_API, SERVER_URL } from 'src/constants/constant';
+import { createKey, fetchData } from 'src/utils/utils';
 import IssuesItem from './IssuesItem';
 import { IssuesItems, IssuesMainBox } from './IssuesMain.styled';
 import IssuesToolbar from './IssuesToolbar';
 
-const mockData = [
-  {
-    title: '[FE] 이슈 페이지 컴포넌트 구현',
-    avatarUrl: 'https://avatars.githubusercontent.com/u/87521172?s=40&v=4',
-    author: '#25 opened 5 hours ago by hoi',
-  },
-  {
-    title: '[FE] 이슈 아이템 컴포넌트 구현 ',
-    avatarUrl: 'https://avatars.githubusercontent.com/u/87521172?s=40&v=4',
-    author: '#24 opened 5 hours ago by hoi',
-  },
-  {
-    title: '[FE] 이슈 아이템 데이터 생성',
-    avatarUrl: 'https://avatars.githubusercontent.com/u/87521172?s=40&v=4',
-    author: '#23 opened 5 hours ago by hoi',
-  },
-  {
-    title: '[FE] CORS 에러..',
-    avatarUrl: 'https://avatars.githubusercontent.com/u/87521172?s=40&v=4',
-    author: '#22 opened 5 hours ago by hoi',
-  },
-  {
-    title: '[FE] HTTPS에서 HTTP 어떻게?',
-    avatarUrl: 'https://avatars.githubusercontent.com/u/87521172?s=40&v=4',
-    author: '#21 opened 5 hours ago by hoi',
-  },
-  {
-    title: '[FE] 이슈 페이지 컴포넌트 구현',
-    avatarUrl: 'https://avatars.githubusercontent.com/u/87521172?s=40&v=4',
-    author: '#20 opened 5 hours ago by hoi',
-  },
-  {
-    title: '[FE] 이슈 페이지 컴포넌트 구현',
-    avatarUrl: 'https://avatars.githubusercontent.com/u/87521172?s=40&v=4',
-    author: '#19 opened 5 hours ago by hoi',
-  },
-  {
-    title: '[FE] 이슈 페이지 컴포넌트 구현',
-    avatarUrl: 'https://avatars.githubusercontent.com/u/87521172?s=40&v=4',
-    author: '#18 opened 5 hours ago by hoi',
-  },
-];
+const ISSUES_API_URL = SERVER_URL + ISSUES_API;
+
+type IssueDataType = {
+  title: string;
+  author: any;
+};
 
 export default function IssuesMain() {
+  const [issuesCount, setIssuesCount] = useState({
+    openCount: 0,
+    closedCount: 0,
+  });
+  const [issuesData, setIssuesData] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const { openCount, closedCount, issues } = await fetchData(ISSUES_API_URL);
+      const { content } = issues;
+
+      const issuesData = content.map(({ title, author }: IssueDataType) => {
+        const { userName, avatarUrl } = author;
+        return { title, author: userName, avatarUrl };
+      });
+
+      setIssuesCount({
+        openCount,
+        closedCount,
+      });
+      setIssuesData(issuesData);
+    };
+
+    getData();
+  }, []);
+
+  console.log(issuesData);
+
   return (
     <IssuesMainBox>
-      <IssuesToolbar />
+      <IssuesToolbar issuesCount={issuesCount} />
       <IssuesItems>
-        {mockData.map(({ title, author, avatarUrl }, idx) => (
+        {issuesData.map(({ title, author, avatarUrl }, idx) => (
           <IssuesItem key={createKey(title, idx)} title={title} author={author} avatarUrl={avatarUrl}></IssuesItem>
         ))}
       </IssuesItems>
