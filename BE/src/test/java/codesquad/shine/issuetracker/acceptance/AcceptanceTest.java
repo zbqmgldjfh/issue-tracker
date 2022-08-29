4ca70bcaafabdff6b9e2af03fe637f6d810bc395
jiwoo <zbqmgldjfh@gmail.com>
2022-08-29 16:43:52 +0900

    test(AuthAcceptanceTest): Form 로그인 인수테스트 작성

---
+++ BE/src/test/java/codesquad/shine/issuetracker/acceptance/AcceptanceTest.java
@@ -0,0 +1,26 @@
+package codesquad.shine.issuetracker.acceptance;
+
+import codesquad.shine.issuetracker.utils.DatabaseCleanup;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AcceptanceTest {
+
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.port = port;
+        databaseCleanup.execute();
+    }
+}
