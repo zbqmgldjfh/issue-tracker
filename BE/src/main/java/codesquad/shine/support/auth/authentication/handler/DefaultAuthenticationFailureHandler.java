49cb36c73f7805fe23877f1b90736613c26e414c
jiwoo <zbqmgldjfh@gmail.com>
2022-08-30 11:29:30 +0900

    feat(Handler): Success, Failure Handler 구현

---
+++ BE/src/main/java/codesquad/shine/support/auth/authentication/handler/DefaultAuthenticationFailureHandler.java
@@ -0,0 +1,13 @@
+package codesquad.shine.support.auth.authentication.handler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class DefaultAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, Exception failed) throws IOException {
+
+    }
+}
