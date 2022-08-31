fc13bd4687bc75821986c82cdaa83e9a6d9346c3
jiwoo <zbqmgldjfh@gmail.com>
2022-08-31 20:26:21 +0900

    feat(TokenAuthenticationInterceptor): TokenAuthenticationInterceptor 인증 필터 구현

---
+++ BE/src/main/java/codesquad/shine/support/auth/authentication/filter/TokenAuthenticationInterceptor.java
@@ -0,0 +1,50 @@
+package codesquad.shine.support.auth.authentication.filter;
+
+import codesquad.shine.support.auth.authentication.AuthenticationToken;
+import codesquad.shine.support.auth.authentication.dto.TokenRequest;
+import codesquad.shine.support.auth.authentication.handler.AuthenticationFailureHandler;
+import codesquad.shine.support.auth.authentication.handler.AuthenticationSuccessHandler;
+import codesquad.shine.support.auth.authentication.provider.AuthenticationManager;
+import codesquad.shine.support.auth.context.Authentication;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+public class TokenAuthenticationInterceptor extends AbstractAuthenticationProcessingFilter {
+
+
+    public TokenAuthenticationInterceptor(AuthenticationSuccessHandler successHandler,
+                                          AuthenticationFailureHandler failureHandler,
+                                          AuthenticationManager authenticationManager) {
+        super(successHandler, failureHandler, authenticationManager);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        AuthenticationToken authRequest = convert(request);
+        return this.getAuthenticationManager().authenticate(authRequest);
+    }
+
+    protected AuthenticationToken convert(HttpServletRequest request) throws IOException {
+        String content = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        TokenRequest tokenRequest = new ObjectMapper().readValue(content, TokenRequest.class);
+
+        String principal = tokenRequest.getEmail();
+        String credentials = tokenRequest.getPassword();
+
+        return new AuthenticationToken(principal, credentials);
+    }
+
+    @Override
+    protected boolean getContinueChainBeforeSuccessfulAuthentication() {
+        return false;
+    }
+
+    @Override
+    protected boolean getContinueChainBeforeUnsuccessfulAuthentication() {
+        return false;
+    }
+}
