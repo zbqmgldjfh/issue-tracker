071ee740c23897408eb4c5ddfcefe2cd217c8b70
jiwoo <zbqmgldjfh@gmail.com>
2022-08-31 21:01:58 +0900

    feat(TokenAuthenticationProvider): TokenAuthenticationProvider 구현

---
+++ BE/src/main/java/codesquad/shine/support/auth/authentication/handler/TokenAuthenticationSuccessHandler.java
@@ -0,0 +1,30 @@
+package codesquad.shine.support.auth.authentication.handler;
+
+import codesquad.shine.support.auth.authentication.dto.TokenResponse;
+import codesquad.shine.support.auth.context.Authentication;
+import codesquad.shine.support.auth.token.JwtTokenFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TokenAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+    private JwtTokenFactory jwtTokenFactory;
+
+    public TokenAuthenticationSuccessHandler(JwtTokenFactory jwtTokenFactory) {
+        this.jwtTokenFactory = jwtTokenFactory;
+    }
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        String token = jwtTokenFactory.createToken(authentication.getPrincipal().toString(), authentication.getAuthorities());
+        TokenResponse tokenResponse = new TokenResponse(token);
+
+        String responseToClient = new ObjectMapper().writeValueAsString(tokenResponse);
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getOutputStream().print(responseToClient);
+    }
+}
