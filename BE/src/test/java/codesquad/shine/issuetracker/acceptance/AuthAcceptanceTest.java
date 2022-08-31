c7e83ca0bd4644db0bb10b619a8359d6f8536897
jiwoo <zbqmgldjfh@gmail.com>
2022-08-31 15:47:37 +0900

    test(AuthAcceptanceTest): Bearer로그인 인수테스트 작성

--- BE/src/test/java/codesquad/shine/issuetracker/acceptance/AuthAcceptanceTest.java
+++ BE/src/test/java/codesquad/shine/issuetracker/acceptance/AuthAcceptanceTest.java
@@ -9,6 +9,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -30,7 +33,7 @@ public class AuthAcceptanceTest extends AcceptanceTest {
     @DisplayName("Form 인증방식의 로그인")
     @Test
     public void form_login() {
-        // given
+        // when
         ExtractableResponse<Response> 폼_로그인_응답 = RestAssured
                 .given().log().all()
                 .auth().form(EMAIL, PASSWORD, new FormAuthConfig("/login/form", USERNAME_FIELD, PASSWORD_FIELD))
@@ -40,7 +43,7 @@ public class AuthAcceptanceTest extends AcceptanceTest {
                 .statusCode(HttpStatus.OK.value())
                 .extract();
 
-        // when, then
+        // then
         assertAll(
                 () -> assertThat(폼_로그인_응답.jsonPath().getString("id")).isNotNull(),
                 () -> assertThat(폼_로그인_응답.jsonPath().getString("email")).isEqualTo(EMAIL),
@@ -48,10 +51,16 @@ public class AuthAcceptanceTest extends AcceptanceTest {
         );
     }
 
+    /**
+     * Given 이미 가입된 사용자가 있고
+     * And 아직 로그인하지 않은 상태일때
+     * When Basic방식으로 로 로그인을 시도하면
+     * Then 성공적으로 로그인된다
+     */
     @DisplayName("Basic 인증방식의 로그인")
     @Test
     public void basic_login() {
-        // given
+        // when
         ExtractableResponse<Response> 베이직_로그인_응답 = RestAssured.given().log().all()
                 .auth().preemptive().basic(EMAIL, PASSWORD)
                 .accept(MediaType.APPLICATION_JSON_VALUE)
@@ -60,11 +69,52 @@ public class AuthAcceptanceTest extends AcceptanceTest {
                 .statusCode(HttpStatus.OK.value())
                 .extract();
 
-        // when
+        // then
         assertAll(
                 () -> assertThat(베이직_로그인_응답.jsonPath().getString("id")).isNotNull(),
                 () -> assertThat(베이직_로그인_응답.jsonPath().getString("email")).isEqualTo(EMAIL),
                 () -> assertThat(베이직_로그인_응답.jsonPath().getString("name")).isEqualTo(NAME)
+        );
+    }
+
+    /**
+     * Given 이미 가입된 사용자가 있고
+     * And 아직 로그인하지 않은 상태일때
+     * When Bearer 로 로그인을 시도하면
+     * Then 성공적으로 로그인된다
+     */
+    @DisplayName("Bearer 인증방식의 로그인")
+    @Test
+    public void bearer_login() {
+        // given
+        Map<String, String> params = new HashMap<>();
+        params.put("email", EMAIL);
+        params.put("password", PASSWORD);
+
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .body(params)
+                .when()
+                .post("/login/token")
+                .then().log().all()
+                .extract();
+        String accessToken = response.jsonPath().getString("accessToken");
+
+        // when
+        ExtractableResponse<Response> 토큰_로그인_응답 = RestAssured.given().log().all()
+                .auth().oauth2(accessToken)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/api/users/me")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract();
+
+        // then
+        assertAll(
+                () -> assertThat(토큰_로그인_응답.jsonPath().getString("id")).isNotNull(),
+                () -> assertThat(토큰_로그인_응답.jsonPath().getString("email")).isEqualTo(EMAIL),
+                () -> assertThat(토큰_로그인_응답.jsonPath().getString("name")).isEqualTo(NAME)
         );
     }
 }
