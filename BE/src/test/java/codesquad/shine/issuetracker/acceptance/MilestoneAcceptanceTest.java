dd57c7bb6ede8f939e21258217a37e49e2e2841b
jiwoo <zbqmgldjfh@gmail.com>
2022-09-05 20:33:24 +0900

    refactor(MilestoneAcceptanceTest): Milestone 인수테스트 리팩터링과 Step메서드 분리

--- BE/src/test/java/codesquad/shine/issuetracker/acceptance/MilestoneAcceptanceTest.java
+++ BE/src/test/java/codesquad/shine/issuetracker/acceptance/MilestoneAcceptanceTest.java
@@ -1,19 +1,20 @@
 package codesquad.shine.issuetracker.acceptance;
 
-import codesquad.shine.issuetracker.milestone.domain.Milestone;
 import codesquad.shine.issuetracker.milestone.dto.request.MilestoneCreateRequest;
 import codesquad.shine.issuetracker.milestone.dto.request.MilestoneEditRequest;
-import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 
 import java.time.LocalDate;
 
 import static codesquad.shine.issuetracker.acceptance.AuthSteps.로그인_되어_있음;
-import static org.assertj.core.api.Assertions.assertThat;
+import static codesquad.shine.issuetracker.acceptance.MilestoneSteps.마일스톤_삭제_요청;
+import static codesquad.shine.issuetracker.acceptance.MilestoneSteps.마일스톤_생성_요청;
+import static codesquad.shine.issuetracker.acceptance.MilestoneSteps.마일스톤_수정_요청;
+import static codesquad.shine.issuetracker.acceptance.MilestoneSteps.마일스톤_아이디_조회;
+import static codesquad.shine.issuetracker.acceptance.MilestoneSteps.마일스톤_응답_확인;
+import static codesquad.shine.issuetracker.acceptance.MilestoneSteps.마일스톤_제목_수정_확인;
+import static codesquad.shine.issuetracker.acceptance.MilestoneSteps.마일스톤_조회_요청;
 
 public class MilestoneAcceptanceTest extends AcceptanceTest {
 
@@ -31,63 +32,21 @@ public class MilestoneAcceptanceTest extends AcceptanceTest {
      */
     @Test
     public void milestone_crud_test() {
-        Milestone milestone = new Milestone(1L, "milestone", "test입니다", LocalDate.now(), true);
+        // given
         MilestoneCreateRequest createRequest = new MilestoneCreateRequest("milestone", "테스트 입니다", LocalDate.now());
-
         String accessToken = 로그인_되어_있음(MEMBER_EMAIL, PASSWORD);
-        ExtractableResponse<Response> response = RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .auth().oauth2(accessToken)
-                .body(createRequest)
-                .when()
-                .post("/api/milestones")
-                .then().log().all()
-                .extract();
+
+        var response = 마일스톤_생성_요청(createRequest, accessToken);
         마일스톤_응답_확인(response, HttpStatus.CREATED);
 
-        ExtractableResponse<Response> 마일스톤_조회_응답 = RestAssured
-                .given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .auth().oauth2(accessToken)
-                .param("status", "open")
-                .when()
-                .get("/api/milestones")
-                .then().log().all()
-                .extract();
+        var 마일스톤_조회_응답 = 마일스톤_조회_요청(accessToken);
         String mileStoneId = 마일스톤_아이디_조회(마일스톤_조회_응답);
 
         MilestoneEditRequest editRequest = new MilestoneEditRequest("change", "설명입니다.", LocalDate.now());
-        ExtractableResponse<Response> 마일스톤_수정_응답 = RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .auth().oauth2(accessToken)
-                .body(editRequest)
-                .when()
-                .patch("/api/milestones/" + mileStoneId)
-                .then().log().all()
-                .extract();
+        var 마일스톤_수정_응답 = 마일스톤_수정_요청(accessToken, mileStoneId, editRequest);
         마일스톤_제목_수정_확인(마일스톤_수정_응답, "change");
 
-        ExtractableResponse<Response> 마일스톤_삭제_응답 = RestAssured
-                .given().log().all()
-                .auth().oauth2(accessToken)
-                .when()
-                .delete("/api/milestones/" + mileStoneId)
-                .then().log().all()
-                .extract();
+        var 마일스톤_삭제_응답 = 마일스톤_삭제_요청(accessToken, mileStoneId);
         마일스톤_응답_확인(마일스톤_삭제_응답, HttpStatus.NO_CONTENT);
-    }
-
-    public void 마일스톤_응답_확인(ExtractableResponse<Response> response, HttpStatus httpStatus) {
-        assertThat(response.statusCode()).isEqualTo(httpStatus.value());
-    }
-
-    public String 마일스톤_아이디_조회(ExtractableResponse<Response> 마일스톤_조회_요청_응답) {
-        return 마일스톤_조회_요청_응답.jsonPath().getString("milestones[0].id");
-    }
-
-    public void 마일스톤_제목_수정_확인(ExtractableResponse<Response> 마일스톤_수정_요청_응답, String editedTitle) {
-        assertThat(마일스톤_수정_요청_응답.jsonPath().getString("title")).isEqualTo(editedTitle);
     }
 }
