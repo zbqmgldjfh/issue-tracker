5b373d6630749796704b3d96380fb4378877a58f
jiwoo <zbqmgldjfh@gmail.com>
2022-09-05 20:19:28 +0900

    chore(LabelAcceptanceTest): 필요없는 setUp() 제거

--- BE/src/test/java/codesquad/shine/issuetracker/acceptance/LabelAcceptanceTest.java
+++ BE/src/test/java/codesquad/shine/issuetracker/acceptance/LabelAcceptanceTest.java
@@ -3,7 +3,6 @@ package codesquad.shine.issuetracker.acceptance;
 import codesquad.shine.issuetracker.label.domain.Color;
 import codesquad.shine.issuetracker.label.domain.Label;
 import codesquad.shine.issuetracker.label.dto.reqeust.LabelEditRequest;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
@@ -11,22 +10,13 @@ import org.springframework.http.HttpStatus;
 import static codesquad.shine.issuetracker.acceptance.AuthSteps.로그인_되어_있음;
 import static codesquad.shine.issuetracker.acceptance.LabelSteps.라벨_삭제_요청;
 import static codesquad.shine.issuetracker.acceptance.LabelSteps.라벨_생성_요청;
-import static codesquad.shine.issuetracker.acceptance.LabelSteps.라벨_생성_응답_확인;
 import static codesquad.shine.issuetracker.acceptance.LabelSteps.라벨_수정_요청;
 import static codesquad.shine.issuetracker.acceptance.LabelSteps.라벨_아이디_조회;
+import static codesquad.shine.issuetracker.acceptance.LabelSteps.라벨_응답_확인;
 import static codesquad.shine.issuetracker.acceptance.LabelSteps.라벨_제목_수정_확인;
 import static codesquad.shine.issuetracker.acceptance.LabelSteps.라벨_조회_요청;
 
 public class LabelAcceptanceTest extends AcceptanceTest {
-
-    Label label;
-
-    @Override
-    @BeforeEach
-    public void setUp() {
-        super.setUp();
-        label = new Label("title", "테스트 입니다", new Color("bg", "fg"));
-    }
 
     /**
      * Scenario: Label 스토리 테스트
@@ -45,10 +35,11 @@ public class LabelAcceptanceTest extends AcceptanceTest {
     @Test
     public void label_crud_test() {
         // given
+        Label label = new Label("title", "테스트 입니다", new Color("bg", "fg"));
         String accessToken = 로그인_되어_있음(MEMBER_EMAIL, PASSWORD);
 
         var 라벨_생성_요청_응답 = 라벨_생성_요청(label, accessToken);
-        라벨_생성_응답_확인(라벨_생성_요청_응답, HttpStatus.CREATED);
+        라벨_응답_확인(라벨_생성_요청_응답, HttpStatus.CREATED);
 
         var 라벨_조회_요청_응답 = 라벨_조회_요청(accessToken);
         String labelId = 라벨_아이디_조회(라벨_조회_요청_응답);
@@ -58,6 +49,6 @@ public class LabelAcceptanceTest extends AcceptanceTest {
         라벨_제목_수정_확인(라벨_수정_요청_응답, "change title");
 
         var 라벨_삭제_요청_응답 = 라벨_삭제_요청(labelId, accessToken);
-        라벨_생성_응답_확인(라벨_삭제_요청_응답, HttpStatus.NO_CONTENT);
+        라벨_응답_확인(라벨_삭제_요청_응답, HttpStatus.NO_CONTENT);
     }
 }
