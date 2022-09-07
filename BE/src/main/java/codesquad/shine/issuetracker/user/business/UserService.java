c8a422e54bb0d91727e3fa52b014cf3298013c6b
jiwoo <zbqmgldjfh@gmail.com>
2022-09-07 21:17:20 +0900

    refactor: null이 넘어올 경우 EmptyList를 반환하도록 변경

--- BE/src/main/java/codesquad/shine/issuetracker/user/business/UserService.java
+++ BE/src/main/java/codesquad/shine/issuetracker/user/business/UserService.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,6 +33,9 @@ public class UserService {
     }
 
     public List<User> getAssigneeInId(List<Long> ids) {
+        if (ids == null) {
+            return Collections.emptyList();
+        }
         return userRepository.findAllById(ids);
     }
 
