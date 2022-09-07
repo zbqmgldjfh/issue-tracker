138eac6fe5505ea3b640eb0526a1dacc9305bad4
jiwoo <zbqmgldjfh@gmail.com>
2022-09-07 21:19:57 +0900

    refactor(IssueService#create): Issue Create 로직 수정
    - 기존 로직은 Lazy로딩 문제로 인하여 반환시 오류가 발생했다.

--- BE/src/main/java/codesquad/shine/issuetracker/config/WebConfig.java
+++ BE/src/main/java/codesquad/shine/issuetracker/config/WebConfig.java
@@ -1,29 +1,13 @@
 package codesquad.shine.issuetracker.config;
 
-import codesquad.shine.issuetracker.auth.interceptor.AuthenticationInterceptor;
 import codesquad.shine.issuetracker.common.StringToStatusConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    private final AuthenticationInterceptor authenticationInterceptor;
-
-    public WebConfig(AuthenticationInterceptor authenticationInterceptor) {
-        this.authenticationInterceptor = authenticationInterceptor;
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(authenticationInterceptor)
-                .order(1)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/", "/auth/**", "/health", "/favicon.ico", "/css/**");
-    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
