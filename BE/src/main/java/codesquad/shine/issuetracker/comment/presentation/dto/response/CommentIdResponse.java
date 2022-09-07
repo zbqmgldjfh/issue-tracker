de8e85b3c224831e5a66bb31dbebff5f3b425134
jiwoo <zbqmgldjfh@gmail.com>
2022-09-07 21:18:52 +0900

    refactor(IssueController): IssueController에서 반환하는 DTO 리팩터링

---
+++ BE/src/main/java/codesquad/shine/issuetracker/comment/presentation/dto/response/CommentIdResponse.java
@@ -0,0 +1,11 @@
+package codesquad.shine.issuetracker.comment.presentation.dto.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CommentIdResponse {
+
+    private final Long commentId;
+}
