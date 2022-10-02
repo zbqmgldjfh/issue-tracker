f1addde8c73acd757d85333cb883a00deb23aa35
jiwoo <zbqmgldjfh@gmail.com>
2022-10-02 15:08:21 +0900

    test(AuthAcceptanceTest): RefreshToken 발급을 위한 인수테스트 작성

--- BE/src/test/java/codesquad/shine/issuetracker/acceptance/AuthAcceptanceTest.java
+++ BE/src/test/java/codesquad/shine/issuetracker/acceptance/AuthAcceptanceTest.java
@@ -1,11 +1,18 @@
 package codesquad.shine.issuetracker.acceptance;
 
+import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static codesquad.shine.issuetracker.acceptance.AuthSteps.로그인_되어_있음;
+import static codesquad.shine.issuetracker.acceptance.AuthSteps.로그인_요청;
 import static codesquad.shine.issuetracker.acceptance.AuthSteps.베어러_인증으로_내_회원_정보_조회_요청;
 import static codesquad.shine.issuetracker.acceptance.AuthSteps.베이직_로그인_후_회원_정보_조회;
 import static codesquad.shine.issuetracker.acceptance.AuthSteps.폼_로그인_후_회원_정보_조회;
@@ -65,6 +72,35 @@ public class AuthAcceptanceTest extends AcceptanceTest {
         회원_정보_조회(베어러_로그인_응답, ADMIN_EMAIL, NAME);
     }
 
+    /**
+     * Scenario: AccessToken이 만료된 경우 RefreshToken을 통해 재발급 받을 수 있다.
+     * Given 사용자가 ROLE_MEMBER로 로그인되어 있다
+     * When RefreshToken을 통해 Bearer Token 인증으로 로그인을 시도하면
+     * Then 신규 AccessToken이 발급된다
+     */
+    @DisplayName("Refresh Token으로 요청할 경우 새로운 AccessToken을 반환한다.")
+    @Test
+    public void bearer_token_login_with_refresh_token() {
+        // given
+        ExtractableResponse<Response> response = 로그인_요청(ADMIN_EMAIL, PASSWORD);
+        String accessToken = response.jsonPath().getString("accessToken");
+        String refreshToken = response.jsonPath().getString("refreshToken");
+
+        // when
+        Map<String, String> params = new HashMap<>();
+        params.put("refreshToken", refreshToken);
+
+        ExtractableResponse<Response> refreshResponse = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(params)
+                .when().post("/login/reissue")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value()).extract();
+
+        // then
+        assertThat(refreshResponse.jsonPath().getString("accessToken")).isNotNull();
+        assertThat(refreshResponse.jsonPath().getString("accessToken")).isNotEqualTo(accessToken);
+    }
 
 
     private void 회원_정보_조회(ExtractableResponse<Response> response, String email, String name) {
